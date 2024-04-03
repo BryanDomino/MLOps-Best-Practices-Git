37d17e699fd300ca7bdab60d90480012de2bec3d
Bryan Prosser <bryan.prosser@dominodatalab.com>
2024-04-03 03:46:47 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/bryan_prosser/MLOps-Demo?executionId=660d2f57399d9148750c3675

--- scripts/sklearn_model_train.py
+++ scripts/sklearn_model_train.py
@@ -18,47 +18,49 @@ from mlflow import MlflowClient
 path = str('/mnt/data/{}/WineQualityData.csv'.format(os.environ.get('DOMINO_PROJECT_NAME')))
 df = pd.read_csv(path)
 print('Read in {} rows of data'.format(df.shape[0]))
-
+ 
 #rename columns to remove spaces
 for col in df.columns:
     df.rename({col: col.replace(' ', '_')}, axis =1, inplace = True)
-
+ 
 #Create is_red variable to store red/white variety as int    
 df['is_red'] = df.type.apply(lambda x : int(x=='red'))
-
+ 
 #Find all pearson correlations of numerical variables with quality
 corr_values = df.corr(numeric_only=True).sort_values(by = 'quality')['quality'].drop('quality',axis=0)
-
+ 
 #Keep all variables with above a 8% pearson correlation
 important_feats=corr_values[abs(corr_values)>0.08]
-
+ 
 #Get data set up for model training and evaluation
-
+ 
 #Drop NA rows
 df = df.dropna(how='any',axis=0)
 #Split df into inputs and target
 X = df[important_feats.keys()]
 y = df['quality'].astype('float64')
-
+ 
 # create a new MLFlow experiemnt
 mlflow.set_experiment(experiment_name=os.environ.get('DOMINO_PROJECT_NAME') + " " + os.environ.get('DOMINO_STARTING_USERNAME'))
-
+ 
 with mlflow.start_run():
     # Set MLFlow tag to differenciate the model approaches
     mlflow.set_tag("Model_Type", "sklearn")
     
     #Create 70/30 train test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-
+    
+    signature = infer_signature(X_test, y_test)
+ 
     #initiate and fit Gradient Boosted Classifier
     print('Training model...')
     gbr = GradientBoostingRegressor(loss='ls',learning_rate = 0.15, n_estimators=75, criterion = 'mse')
     gbr.fit(X_train,y_train)
-
+ 
     #Predict test set
     print('Evaluating model on test data...')
     preds = gbr.predict(X_test)
-
+ 
     #View performance metrics and save them to domino stats!
     print("R2 Score: ", round(r2_score(y_test, preds),3))
     print("MSE: ", round(mean_squared_error(y_test, preds),3))
@@ -66,15 +68,15 @@ with mlflow.start_run():
     # Save the metrics in MLFlow
     mlflow.log_metric("R2", round(r2_score(y_test, preds),3))
     mlflow.log_metric("MSE", round(mean_squared_error(y_test,preds),3))
-
+ 
     #Code to write R2 value and MSE to dominostats value for population in experiment manager
     with open('/mnt/artifacts/dominostats.json', 'w') as f:
         f.write(json.dumps({"R2": round(r2_score(y_test, preds),3),
                            "MSE": round(mean_squared_error(y_test,preds),3)}))
-
+ 
     #Write results to dataframe for visualizations
     results = pd.DataFrame({'Actuals':y_test, 'Predictions':preds})
-
+ 
     print('Creating visualizations...')
     #Add visualizations and save for inspection
     fig1, ax1 = plt.subplots(figsize=(10,6))
@@ -86,31 +88,28 @@ with mlflow.start_run():
         order = 3)
     plt.savefig('/mnt/artifacts/actual_v_pred_scatter.png')
     mlflow.log_figure(fig1, 'actual_v_pred_scatter.png')
-
+ 
     fig2, ax2 = plt.subplots(figsize=(10,6))
     plt.title('Sklearn Actuals vs Predictions Histogram')
     plt.xlabel('Quality')
     sns.histplot(results, bins=6, multiple = 'dodge', palette = 'coolwarm')
     plt.savefig('/mnt/artifacts/actual_v_pred_hist.png')
     mlflow.log_figure(fig2, 'actual_v_pred_hist.png')
-
-    # Log the model in MLflow (new section)
-    model_path = "GradientBoostingRegressorModel"
-    mlflow.sklearn.log_model(gbr, model_path)
-
-    # Register the model in the MLflow Model Registry (new section)
-    model_uri = f"runs:/{mlflow.active_run().info.run_id}/{model_path}"
-    model_name = "WineQualityGradientBoostingRegressor"
-    mlflow.register_model(model_uri, model_name)
     
+ 
+ 
+    #Saving trained model to serialized pickle object 
+    
+    import pickle 
+    
+    # save best model
+    file = '/mnt/code/sklearn_gbm.pkl'
+    pickle.dump(gbr, open(file, 'wb'))
+    
+    mlflow.sklearn.log_model(gbr,
+                             artifact_path="gbr_model",
+                             signature=signature,
+                             )
 mlflow.end_run()
-
-#Saving trained model to serialized pickle object 
-
-import pickle 
-
-# save best model
-file = '/mnt/artifacts/models/sklearn_gbm.pkl'
-pickle.dump(gbr, open(file, 'wb'))
 
 print('Script complete!')
